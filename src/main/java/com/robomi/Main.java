6081cf1953cef1c05e8fa70ded40ded3b6e485e9
jdaddy <jdaddyorer@gmail.com>
2024-05-31 13:20:34 +0900

    window env

--- src/main/java/com/robomi/Main.java
+++ src/main/java/com/robomi/Main.java
@@ -1,20 +1,18 @@
 package com.robomi;
 
+import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "com.robomi.controller",
         "com.robomi.service"
 })
-//@EnableJpaRepositories(basePackages = "com.robomi.repository")
-//@EntityScan(basePackages = "com.robomi.entity")
 public class Main {
     public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         SpringApplication.run(Main.class, args);
     }
 }
