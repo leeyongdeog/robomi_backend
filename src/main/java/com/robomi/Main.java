8626f7daf29804948c318b46ee9faf58c77e3b13
jdaddy <jdaddyorer@gmail.com>
2024-06-10 17:22:53 +0900

    temp

--- src/main/java/com/robomi/Main.java
+++ src/main/java/com/robomi/Main.java
@@ -3,9 +3,13 @@ package com.robomi;
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.opencv.global.opencv_core;
 import org.opencv.core.Core;
+import org.ros.node.Node;
+import org.ros.node.NodeConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.net.URI;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -15,11 +19,22 @@ import org.springframework.context.annotation.ComponentScan;
         "com.robomi"
 })
 public class Main {
+    private static final NodeConfiguration getNodeConfiguration(final String rosHostIP, final String nodeName, final URI rowMasterUri){
+        final NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(rosHostIP);
+        nodeConfiguration.setNodeName(nodeName);
+        nodeConfiguration.setMasterUri(rowMasterUri);
+        return nodeConfiguration;
+    }
+
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("OpenCV loaded successfully. Version: " + Core.VERSION);
+
         Loader.load(opencv_core.class);
         System.out.println("JavaCV native libraries loaded successfully.");
+
+
+
         SpringApplication.run(Main.class, args);
     }
 }
--- src/main/java/com/robomi/ros/PubAudioNode.java
+++ src/main/java/com/robomi/ros/PubAudioNode.java
@@ -1,4 +1,52 @@
 package com.robomi.ros;
 
-public class PubAudioNode {
+import audio_common_msgs.AudioData;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.Unpooled;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.Node;
+import org.ros.node.topic.Publisher;
+
+public class PubAudioNode extends AbstractNodeMain {
+    private final String topicName;
+    private final String nodeName;
+    private static boolean isRunning = false;
+
+    public PubAudioNode(String topicName, String nodeName){
+        this.topicName = topicName;
+        this.nodeName = nodeName;
+    }
+
+    @Override
+    public GraphName getDefaultNodeName() {
+        return GraphName.of(this.nodeName);
+    }
+
+    @Override
+    public void onStart(ConnectedNode connectedNode) {
+        isRunning = true;
+        final Publisher<AudioData> publisher = connectedNode.newPublisher(topicName,nodeName);
+
+        new Thread(() -> {
+            while(isRunning){
+                byte[] audioData = new byte[0]; // 클라이언트에서 받은 데이터
+                AudioData data = publisher.newMessage();
+                ChannelBuffer channelBuffer = ChannelBuffers.wrappedBuffer(audioData);
+                data.setData(channelBuffer);
+                publisher.publish(data);
+            }
+        }).start();
+        super.onStart(connectedNode);
+    }
+
+    @Override
+    public void onShutdown(Node node) {
+        isRunning = false;
+        super.onShutdown(node);
+    }
 }
